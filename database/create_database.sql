04cd5d1c799eeadbd892b82616c62d6f16da5023
arseniit <arseniit@gmail.com>
2024-02-07 00:38:49 +0100

    Add SQL mock data for showing the result

--- database/create_database.sql
+++ database/create_database.sql
@@ -5,3 +5,17 @@ CREATE USER IF NOT EXISTS 'dto'@'localhost' IDENTIFIED BY 'dto';
 
 GRANT ALL PRIVILEGES ON `wcsdto`.* TO 'dto'@'localhost';
 FLUSH PRIVILEGES;
+
+-- mock data for the pokemon and user tables
+
+INSERT INTO user (name, age, level) VALUES
+                                        ('Ash Ketchum', 10, 100),
+                                        ('Misty', 12, 80);
+
+INSERT INTO pokemon (name, power, attribute) VALUES
+                                                  ('Pikachu', 8, 'FIRE'),
+                                                  ('Charmander', 7, 'ELECTRICITY'),
+                                                  ('Squirtle', 7, 'ICE'),
+                                                  ('Bulbasaur', 7, 'FIRE'),
+                                                  ('Jigglypuff', 6, 'WIND');
+
